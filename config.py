bcf419547a803331805819bc59044501e1e93541
ManuelS <74248613+MaSchm1983@users.noreply.github.com>
2025-11-25 01:17:33 +0100

    final config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from PyQt5.QtGui import QColor, QFont
 
 # Window & frame sizes
 WINDOW_WIDTH = 400
-MIN_HEIGHT = 460  # Increased height by 50
-MAX_HEIGHT = 460
+MIN_HEIGHT = 500  # Increased height by 50
+MAX_HEIGHT = 500
 TITLE_BAR_HEIGHT = 35
 BUTTON_SIZE = 25
 FRAME_PADDING = 5
@@ -15,6 +15,7 @@ BAR_HEIGHT = 20
 BAR_PADDING = 10
 STARTSTOP_BTN_WIDTH = 80
 STARTSTOP_BTN_HEIGHT = 25
+AUTO_STOP_SECONDS = 30
 
 
 # Fonts
@@ -27,21 +28,14 @@ FONT_TEXT_CLS_BTN = QFont('Arial', 14, QFont.Bold)
 # Colors
 COLORS = {
     'background':          QColor(0, 0, 0, int(0.5*255)),
-    'background_dps':      QColor(210, 180, 180, int(0.35*255)),
-    'background_hps':      QColor(180, 210, 180, int(0.35*255)),
-    'background_dts':      QColor(180, 180, 210, int(0.35*255)),
-    'MODE_BTN_BG_DPS':     QColor(255, 100, 100, int(0.4*255)),
-    'MODE_BTN_BG_HPS':     QColor(100, 255, 100, int(0.4*255)),
-    'MODE_BTN_BG_DTS':     QColor(100, 100, 255, int(0.4*255)),
+    'MODE_BTN_BG_DPS':     QColor(255, 100, 100, int(0.3*255)),
+    'MODE_BTN_BG_HPS':     QColor(100, 255, 100, int(0.3*255)),
+    'MODE_BTN_BG_DTS':     QColor(100, 100, 255, int(0.3*255)),
     'title_bar':       QColor(70, 50,  30, int(0.8*255)),
     'title_bar_dps':       QColor(160, 80,  80, int(0.6*255)),
     'title_bar_hps':       QColor(80,  160,  80, int(0.6*255)),
     'title_bar_dts':       QColor(80,  80,  160, int(0.6*255)),
     'line_col':      QColor( 50,  50,  50, int(0.8*255)),
-    'frame_bg':        QColor(100, 100, 100, int(0.55*255)),
-    #'frame_bg':        QColor(255, 220, 220, int(0.5*255)),
-    'bar_bg':          QColor(170, 170, 170, int(0.55*255)),
-    'bar_fill':        QColor(200,  65,  65, int(0.6*255)),
     'button_active':   QColor(80,  200,  80, int(0.5*255)),
     'button_noactive': QColor(200,  80,  80, int(0.5*255)),
     'text':            QColor( 25,  25,  25),
@@ -53,5 +47,11 @@ AA_SKILLS = {
     "Dual-wield Attack",
     "Bow Attack",
     "Weapon Attack",
-    "1H Weapon/Shield Attack"
+    "1H Weapon/Shield Attack",
+    "2H Weapon Attack",
+    "Bogenangriff",
+    "1H-Waffen-/Schild-Angriff",
+    "Doppelangriff",
+    "2H-Waffen-Angriff",
+    "Waffen-Angriff",
 }
